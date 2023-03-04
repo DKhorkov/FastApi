c78bcd0888f3af185d428dd25af233822a246506
DKhorkov <dkhorkov.dev@gmail.com>
2023-03-04 17:38:14 +0300

    Removed learning files

--- Task_Manager/base_api/base_api_exceptions.py
+++
@@ -1,6 +0,0 @@
-class InvalidTypeException(Exception):
-    """Raises, when FastAPi Query value can not be transferred from string to necessary type"""
-
-    def __init__(self, type_to_transfer):
-        self.message = f'Query value can not be transferred to {type_to_transfer} type!'
-        super().__init__(self.message)
